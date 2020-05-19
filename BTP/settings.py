858474806149600615340fc0de028d8c00d0fd95
sagar saini <chasesagar@gmail.com>
2020-05-19 20:03:42 +0530

    changing whitenoise position csrf error

--- BTP/settings.py
+++ BTP/settings.py
@@ -43,13 +43,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', #new whitenoise for depolyment
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #new whitenoise for depolyment
 ]
 
 ROOT_URLCONF = 'BTP.urls'
